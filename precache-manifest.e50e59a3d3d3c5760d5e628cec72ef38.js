0a9fb61843106d306a687a74b4df69ca368a6b41
SebastienBosca <sbosca@wanadoo.fr>
2022-05-04 20:27:43 +0200

    Updates

---
+++ precache-manifest.e50e59a3d3d3c5760d5e628cec72ef38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d260f6097e42b0c76c69abcf73d7c0cd",
+    "url": "/defi4s/index.html"
+  },
+  {
+    "revision": "83f25c69349b90000d47",
+    "url": "/defi4s/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "80fdfff9060d2030213c",
+    "url": "/defi4s/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "83f25c69349b90000d47",
+    "url": "/defi4s/static/js/2.88da5d28.chunk.js"
+  },
+  {
+    "revision": "80fdfff9060d2030213c",
+    "url": "/defi4s/static/js/main.5612d297.chunk.js"
+  },
+  {
+    "revision": "ddd971b0976b2d923ef1",
+    "url": "/defi4s/static/js/runtime-main.7118d1ca.js"
+  }
+]);
